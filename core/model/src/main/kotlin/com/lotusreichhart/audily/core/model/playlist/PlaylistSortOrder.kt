d717623aa430a0cd1bec0d2df52bb034138b6ee0
LotusReichhart <reichhartlotus@gmail.com>
2026-04-23 16:06:29 +0700

    core(model): add and change core model for song and playlist

---
+++ core/model/src/main/kotlin/com/lotusreichhart/audily/core/model/playlist/PlaylistSortOrder.kt
@@ -0,0 +1,11 @@
+package com.lotusreichhart.audily.core.model.playlist
+
+/**
+ * Tiêu chí sắp xếp danh sách Playlist.
+ */
+enum class PlaylistSortOrder {
+    NAME_ASC,
+    NAME_DESC,
+    CREATED_DATE_ASC,
+    CREATED_DATE_DESC
+}
