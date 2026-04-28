9a1859a8916d24669a8326f0129d33f38d4e9be4
LotusReichhart <reichhartlotus@gmail.com>
2026-04-28 20:36:40 +0700

    feat(designsystem): add playback icons, duration utils and update artwork component

--- core/designsystem/src/main/kotlin/com/lotusreichhart/audily/core/designsystem/resource/AudilyIcons.kt
+++ core/designsystem/src/main/kotlin/com/lotusreichhart/audily/core/designsystem/resource/AudilyIcons.kt
@@ -3,13 +3,17 @@ package com.lotusreichhart.audily.core.designsystem.resource
 import com.lotusreichhart.audily.core.designsystem.R
 
 object AudilyIcons {
+    val ArrowDown = R.drawable.core_designsystem_ic_arrow_down
     val HomeOutline = R.drawable.core_designsystem_ic_outline_home
     val HomeFill = R.drawable.core_designsystem_ic_fill_home
     val TimerOutline = R.drawable.core_designsystem_ic_outline_timelapse
     val TimerFill = R.drawable.core_designsystem_ic_fill_timelapse
     val SettingsOutline = R.drawable.core_designsystem_ic_outline_settings
     val SettingsFill = R.drawable.core_designsystem_ic_fill_settings
+    val FavoriteOutline = R.drawable.core_designsystem_ic_outline_favorite
+    val FavoriteFill = R.drawable.core_designsystem_ic_fill_favorite
     val DragHandle = R.drawable.core_designsystem_ic_drag_handle
+    val QueueMusic = R.drawable.core_designsystem_ic_queue_music
     val Search = R.drawable.core_designsystem_ic_search
     val Sort = R.drawable.core_designsystem_ic_sort
     val Timer = R.drawable.core_designsystem_ic_timer
