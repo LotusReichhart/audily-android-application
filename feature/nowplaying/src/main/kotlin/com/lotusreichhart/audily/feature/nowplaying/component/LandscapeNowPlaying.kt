aaeddd06287a2c4ba306f916f42a5815d45f5459
LotusReichhart <reichhartlotus@gmail.com>
2026-05-06 19:20:02 +0700

    refactor(nowplaying): apply compact, landscape, and expanded adaptive layouts

--- feature/nowplaying/src/main/kotlin/com/lotusreichhart/audily/feature/nowplaying/component/LandscapeNowPlaying.kt
+++ feature/nowplaying/src/main/kotlin/com/lotusreichhart/audily/feature/nowplaying/component/LandscapeNowPlaying.kt
@@ -14,14 +14,10 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.statusBarsPadding
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -42,7 +38,7 @@ import com.lotusreichhart.audily.feature.nowplaying.NowPlayingUiState
 
 @OptIn(ExperimentalSharedTransitionApi::class)
 @Composable
-internal fun NowPlayingScreenLandscape(
+internal fun LandscapeNowPlaying(
     modifier: Modifier = Modifier,
     uiState: NowPlayingUiState,
     onLyricsToggle: () -> Unit,
