8c136d20cebb4f1b9ec9ec12e4936ab319e22d77
LotusReichhart <reichhartlotus@gmail.com>
2026-05-26 09:25:59 +0700

    upgrade version code

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,8 +20,8 @@ android {
     namespace = "com.lotusreichhart.audily"
     defaultConfig {
         applicationId = "com.lotusreichhart.audily"
-        versionCode = 3
-        versionName = "1.0.1"
+        versionCode = 4
+        versionName = "1.0.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
